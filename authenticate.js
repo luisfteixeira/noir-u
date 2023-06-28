4352f8adb9b1717b566d9d17671d93e136f683e9
Luís Teixeira <84652052+luisfteixeira@users.noreply.github.com>
2023-06-27 21:39:33 -0300

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -1,13 +1,14 @@
+var bcrypt = dcodeIO.bcrypt;
+
 function authenticate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-
-    // Carrega os dados dos usuários do arquivo JSON
+    
     fetch('users.json')
         .then(response => response.json())
         .then(data => {
-            // Encontra o usuário correspondente pelo nome de usuário
-            var user = data.users.find(user => user.username === username);
+            
+            var user = data.users.find(user => user.username === username); 
 
             if (user && bcrypt.compareSync(password, user.password)) {
                 window.location.href = "sistem.html";
